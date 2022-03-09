cd41e7fff8e55134db98658cacd0c64f5bc802dd
arps18 <arpanp99@gmail.com>
2022-03-09 16:24:58 +0530

    About us added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import About from "./components/About";
 import Card from "./components/Card";
 
 function App() {
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
       <Card />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="max-w-4xl mt-20 mx-auto">
+      <p className="text-2xl text-black md:text-4xl font-bold text-center">
+        Hey 👋
+      </p>
+      <p className="text-base text-gray-600 leading-relaxed mt-4 md:text-xl  text-center">
+        I develop awesome Websites using HTML,CSS,Javascript, Applications using
+        Flutter & everything in between. Contributing to OpenSource is my
+        favourite activity! I enjoy Public Speaking and had a few as well! I
+        love to pen down ocassionally Blogs as well!
+      </p>
+    </div>
+  );
+}
+
+export default About;
