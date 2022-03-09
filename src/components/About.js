df4689ebfc25e1da49fc568e95ff3b80fa8018e2
arps18 <arpanp99@gmail.com>
2022-03-09 16:29:36 +0530

    Scroll into view lib added

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ScrollIntoView from "react-scroll-into-view";
+import { FaChevronDown } from "react-icons/fa";
 
 function About() {
   return (
@@ -12,6 +14,11 @@ function About() {
         favourite activity! I enjoy Public Speaking and had a few as well! I
         love to pen down ocassionally Blogs as well!
       </p>
+      <ScrollIntoView selector="#tech">
+        <div className="mx-auto p-20">
+          <FaChevronDown className="animate-bounce mx-auto text-3xl text-blue-300" />
+        </div>
+      </ScrollIntoView>
     </div>
   );
 }
